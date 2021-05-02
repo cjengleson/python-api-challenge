689e1d8cad53357e8fb86d7a9fcbd3901b004258
Carley Engleson <cjengleson@gmail.com>
2021-05-02 10:35:24 -0700

    first hw push

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "0bf08302a865ae52dc2c0854e0fc2a1b"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBwwdUV0hqXI0vyd4-zNljgQL7UXvTndM0"
